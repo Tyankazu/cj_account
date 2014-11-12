ba0352216e2dfe32ed11dfd56b8b1d703f34d1f3
Your Name <you@example.com>
2014-11-12 18:31:42 +0900

    tt

--- src/jp/example/cj_account/MakeAccountActivity.java
+++ src/jp/example/cj_account/MakeAccountActivity.java
@@ -4,6 +4,10 @@
 package jp.example.cj_account;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -14,8 +18,10 @@ import java.io.PrintWriter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -24,6 +30,10 @@ import android.view.View.OnClickListener;
  *
  */
 public class MakeAccountActivity extends Activity {
+	
+	 
+		
+		
 	 public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.make_account);
@@ -33,39 +43,94 @@ public class MakeAccountActivity extends Activity {
 	        Button button3 = (Button)findViewById(R.id.button3);
 	        button3.setOnClickListener(new OnClickListener() {
 	            public void onClick(View v) {
-	                // Sub 画面を起動
-	                Intent intent = new Intent();
-	                intent.setClassName("jp.example.cj_account", "jp.example.cj_account.MakeProfileActivity");
-	                startActivity(intent);
 	                
-	                EditText et = (EditText)findViewById(R.id.editText1);
-	                EditText et2 = (EditText)findViewById(R.id.editText2);
-		            String s = et.getText().toString();
-		            String s2 = et2.getText().toString();
-		            try{
-		            OutputStream out = openFileOutput("a.txt",MODE_PRIVATE);
-		            PrintWriter writer = new PrintWriter(new OutputStreamWriter(out,"UTF-8"));
-		            writer.append(s);
-		            writer.close();
-		            }catch(IOException e){
-		            e.printStackTrace();
-		            }
+	            	//Intent intent2 = new Intent(MakeAccountActivity.this, MakeProfileActivity.class);    
+	                
+	            	EditText et = (EditText)findViewById(R.id.editText1);
+	           	     EditText et2 = (EditText)findViewById(R.id.editText2);
+	           	     String s = null;
+	           	     s =et.getText().toString();
+	           	     String s2 = null;
+	           	     s2 = et2.getText().toString();
+	           	     String s3 = s + s2;
+	           	     String txt = ".txt";
+	           	      String filename = s3 + txt;
 		            
-		            try{
-		            	InputStream in = openFileInput("a.txt");
-		            	BufferedReader reader = new BufferedReader(new InputStreamReader(in,"UTF-8"));
-			            EditText et3 = (EditText)findViewById(R.id.Test);
-		            	 while((s = reader.readLine())!= null){
-		            	 et3.append(s + s2);
-		            	 et3.append("\n");
-		            	 reader.close();
-		            	 }
-		            	}catch(IOException e){
-		            	 e.printStackTrace();
-		            	}
+		                if(s.length() <1  || s2.length() < 8){
+		                	Toast.makeText(getBaseContext(), "Notsavefile", Toast.LENGTH_LONG).show();
+		                }else {
+		                	// Sub 画面を起動
+			                Intent intent = new Intent();
+			                intent.setClassName("jp.example.cj_account", "jp.example.cj_account.MakeProfileActivity");
+			                //startActivity(intent);
+			                
+			                
+		           	   intent.putExtra("FILENAME", filename);
+			               startActivity(intent);
+			                
+		                	String sdcardPath = Environment.getExternalStorageDirectory().getPath();
+				            File file = new File(sdcardPath + "/" + filename);		            
+				            file.getParentFile().mkdir();
+				     
+				            FileOutputStream fos;
+			            try {
+				                fos = new FileOutputStream(file, true);
+				                OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+				                BufferedWriter bw = new BufferedWriter(osw);
+				                //String str = ((EditText) findViewById(R.id.EditTextInput)).getText().toString();
+//				                bw.write(s3);
+				                bw.flush();
+				                bw.close();
+				                
+			            } catch (Exception e) {
+			            	Toast.makeText(getBaseContext(), "Notsavefile", Toast.LENGTH_LONG).show();
+			            }
+		                Toast.makeText(getBaseContext(), "savefile", Toast.LENGTH_LONG).show();
+		                }
+		            
+	            
+	            
+		            
+		            
+//            try{
+//		            FileOutputStream out = openFileOutput(filename,MODE_PRIVATE);
+//		            OutputStreamWriter writer = new OutputStreamWriter(out);
+//		            //PrintWriter writer = new PrintWriter(new OutputStreamWriter(out,"UTF-8"));
+//		            writer.write(s3);
+//		            writer.flush();
+//		            writer.close();
+//		            
+//		          
+//		            } catch (FileNotFoundException e) {
+//		              
+//		              e.printStackTrace();
+//		            }catch(IOException e){
+//		           
+//		            e.printStackTrace();
+//		            }
+		            
+//		            try{
+//		            	InputStream in = openFileInput("filename");
+//		            	BufferedReader reader = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+//			            EditText et3 = (EditText)findViewById(R.id.Test);
+//			            
+//		            	 while((s3 = reader.readLine())!= null){
+//		            	 et3.append(s3);
+//		            	 et3.append("\n");
+//		            	 reader.close();
+//		            	 }
+//		            	}catch(IOException e){
+//		            	 e.printStackTrace();
+//		            	}
+	            
+	            
+	            
 	            }
 		        	            
 	        });
-	           
+	     
 	        }
+	
+	
+	 
 }
--- src/jp/example/cj_account/MakeProfileActivity.java
+++ src/jp/example/cj_account/MakeProfileActivity.java
@@ -3,18 +3,33 @@
  */
 package jp.example.cj_account;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * @author b1012234
  *
  */
 public class MakeProfileActivity extends Activity {
+	//MakeAccountActivity maa =new MakeAccountActivity();
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.makeprofile);
@@ -23,12 +38,86 @@ public class MakeProfileActivity extends Activity {
         Button button4 = (Button)findViewById(R.id.button4);
         button4.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
+            	 String str10 = ((EditText) findViewById(R.id.editText10)).getText().toString();
+                 String str11 = ((EditText) findViewById(R.id.editText11)).getText().toString();
+                 String str12 = ((EditText) findViewById(R.id.editText12)).getText().toString();
+                 String str13 = ((EditText) findViewById(R.id.editText13)).getText().toString();
+                 String str14 = ((EditText) findViewById(R.id.editText14)).getText().toString();
+            	if(str10.length() <1  || str11.length() < 1 || str12.length() < 1 || str13.length() < 1 || str14.length() < 1){
+                	Toast.makeText(getBaseContext(), "Notsavefile", Toast.LENGTH_LONG).show();
+                }else {
+            	
                 // Sub 画面を起動
                 Intent intent = new Intent();
                 intent.setClassName("jp.example.cj_account", "jp.example.cj_account.SubTopActivity");
                 startActivity(intent);
                 finish();
-            }
+                 
+                Intent intent2 =getIntent();
+                String filename2 = intent2.getStringExtra("FILENAME");
+	       	      //String filename2 = "test";
+               String filePath = Environment.getExternalStorageDirectory().getPath() + "/"+ filename2;
+                System.out.println(filename2);
+                
+                FileOutputStream fos = null;
+            	BufferedWriter bw = null;
+                
+                File file = new File(filePath);
+               // file.getParentFile().mkdir();
+
+               try {
+            	   fos = new FileOutputStream(file);
+                    //FileOutputStream fos = openFileOutput(filePath,MODE_APPEND);
+                    //fos = new FileOutputStream(file, true);
+                    OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+                    bw = new BufferedWriter(osw);
+
+                    //String str = "アプリケーションデータ領域への書き込み\nテストデータ";
+                    //writer.write(str);
+                	 
+                  bw.write(str10);
+                  bw.write("\n");
+                  bw.write(str11);
+                  bw.write("\n");
+                  bw.write(str12);
+                  bw.write("\n");
+                  bw.write(str13);
+                  bw.write("\n");
+                  bw.write(str14);
+ //                 bw.write("\n");
+//                  System.out.println("書き込みました");
+                  bw.flush();
+                   bw.close();
+                   Toast.makeText(getBaseContext(), "savefile", Toast.LENGTH_LONG).show();
+                } catch (FileNotFoundException e) {
+                	System.out.println("ファイルありません");
+                } catch (IOException e) {
+                	System.out.println("IO");
+               }
+                
+//                try {
+//                	FileOutputStream stream = openFileOutput(filePath,MODE_PRIVATE);
+//                    OutputStreamWriter bw = new OutputStreamWriter(stream, "UTF-8");
+//                    
+//                	bw.write(str10);
+//                    bw.write("\n");
+//                    bw.write(str11);
+//                    bw.write("\n");
+//                    bw.write(str12);
+//                    bw.write("\n");
+//                    bw.write(str13);
+//                    bw.write("\n");
+//                    bw.write(str14);
+//                    bw.write("\n");
+//                    
+//                    stream.close();
+//                    bw.close();
+//                    Toast.makeText(getBaseContext(), "savefile", Toast.LENGTH_LONG).show();
+//                } catch (FileNotFoundException e) {
+//                } catch (Exception e) {
+//                }
+                }
+           }
         });
     }
 
